906897165f0a3815373391950beb051e70326a14
Brian <bclarkx2@verizon.net>
2017-08-08 13:19:59 -0400

    Add setup script

---
+++ setup.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+###############################################################################
+# Imports                                                                     #
+###############################################################################
+
+import json
+import os
+import git
+import subprocess
+import shutil
+
+
+###############################################################################
+# Constants                                                                   #
+###############################################################################
+
+setup_filepath = "setup.json"
+required_keys = "name", "repo", "location"
+
+
+###############################################################################
+# Class definitions                                                           #
+###############################################################################
+
+class SetupRepo(object):
+
+    def __init__(self, attr_dict):
+        super(SetupRepo, self).__init__()
+        self.__dict__.update(attr_dict)
+        self.location = os.path.expanduser(self.location)
+
+    @staticmethod
+    def of(attr_dict):
+        if not all(key in attr_dict for key in required_keys):
+            raise ValueError("Missing key!")
+        return SetupRepo(attr_dict)
+
+    def yes_no(self, question):
+        reply = input("{} (y/n):".format(question)).lower().strip()
+        if reply.startswith("y"):
+            return True
+        elif reply.startswith("n"):
+            return False
+        else:
+            return self.yes_no(question)
+
+    def okay_to_write(self):
+        if os.path.isdir(self.location):
+            return self.yes_no("Overwrite {}?".format(self.location))
+        else:
+            return True
+
+    def msg(self, format_str):
+        formatted = format_str.format(**self.__dict__)
+        print(formatted)
+
+    def unpack(self):
+
+        if not self.okay_to_write():
+            self.msg("Skipping {name}")
+            return
+
+        clear_dir(self.location)
+        git.Repo.clone_from(self.repo, self.location)
+
+        self.msg("Success! Cloned {name} to {location}")
+
+        if self.script:
+            self.msg("Executing script {script}")
+            full_script = os.path.join(self.location, self.script)
+            subprocess.call([full_script])
+
+
+###############################################################################
+# Helper functions                                                            #
+###############################################################################
+
+def read_json_file(filepath):
+    with open(filepath) as json_file:
+        return json.load(json_file)
+
+
+def clear_dir(directory):
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+
+
+###############################################################################
+# Main script                                                                 #
+###############################################################################
+
+def main():
+
+    repo_dicts = read_json_file(setup_filepath)
+
+    repos = [SetupRepo.of(repo_dict) for repo_dict in repo_dicts if repo_dict["enabled"]]
+
+    for repo in repos:
+        try:
+            repo.unpack()
+        except git.exc.GitError as err:
+            print(err)
+
+
+if __name__ == '__main__':
+    main()
