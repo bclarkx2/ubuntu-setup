f72195f6af85b3febed7aafbc6be35357329c6cc
Brian Clark <bclarkx2@gmail.com>
2017-08-09 22:12:00 -0400

    Add linux pkg list and python pkg list for easy setup

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@
 
 import json
 import os
-import git
 import subprocess
 import shutil
 
@@ -17,6 +16,14 @@ import shutil
 
 setup_filepath = "setup.json"
 required_keys = "name", "repo", "location"
+
+pkgs_dir = "pkgs"
+
+pkgs_filepath = os.path.join(pkgs_dir, "pkgs")
+ignore_pkgs_filepath = os.path.join(pkgs_dir, "ignore_pkgs")
+python_pkgs_filepath = os.path.join(pkgs_dir, "python_pkgs")
+
+SEP = "{:*^30}".format("")
 
 
 ###############################################################################
@@ -68,6 +75,47 @@ class SetupRepo(object):
             subprocess.call([full_script])
 
 
+class Package(object):
+
+    def __init__(self, pkg_name):
+        super(Package, self).__init__()
+        self.pkg_name = pkg_name
+
+    def is_installed(self):
+        return subprocess.check_output(["dpkg", "-s", self.pkg_name])
+
+    def install(self):
+        subprocess.call(["sudo", "apt", "install", self.pkg_name])
+
+    def title(self):
+        title_parts = [SEP, "{:*^30}".format(" " + self.pkg_name + " "), SEP]
+        return "\n".join(title_parts)
+
+    def ensure(self):
+        print()
+        print(self.title())
+        if not self.is_installed():
+            self.install()
+        else:
+            print("Skipping: already installed!".format(self.pkg_name))
+
+
+class PythonPackage(Package):
+
+    def __init__(self, pkg_name):
+        super(PythonPackage, self).__init__(pkg_name)
+
+    def is_installed(self):
+        try:
+            import self.pkg_name
+            return True
+        except ImportError:
+            return False
+
+    def install(self):
+        subprocess.call(["sudo", "pip3", "install", self.pkg_name])
+
+
 ###############################################################################
 # Helper functions                                                            #
 ###############################################################################
@@ -102,14 +150,48 @@ def checkout_submodule_master(submodule):
     repo.heads.master.checkout()
 
 
+def read_pkgs_file(filepath):
+    with open(filepath) as pkgs_file:
+        return [line.rstrip() for line in pkgs_file]
+
+
 ###############################################################################
-# Main script                                                                 #
+# Worker functions                                                            #
 ###############################################################################
 
-def main():
+def update_pkgs():
+    subprocess.call(["sudo", "apt", "update"])
+    subprocess.call(["sudo", "apt", "upgrade"])
 
+
+def install_pkgs():
+
+    # sanity check: gitpython required to install pkgs
+    PythonPackage("gitpython").ensure()
+
+    # do a general update first, to avoid unnecessary work
+    update_pkgs()
+
+    pkgs = read_pkgs_file(pkgs_filepath)
+    ignore_pkgs = read_pkgs_file(ignore_pkgs_filepath)
+
+    install_set = set(pkgs) - set(ignore_pkgs)
+
+    for pkg in install_set:
+        Package(pkg).ensure()
+
+
+def install_python_pkgs():
+
+    # sanity check: pip is required to install python pkgs
+    Package("python3-pip").ensure()
+
+    for pkg in read_pkgs_file(python_pkgs_filepath):
+        PythonPackage(pkg).ensure()
+
+
+def install_repos():
     repo_dicts = read_json_file(setup_filepath)
-
     repos = [SetupRepo.of(repo_dict) for repo_dict in repo_dicts if repo_dict["enabled"]]
 
     for repo in repos:
@@ -117,6 +199,17 @@ def main():
             repo.unpack()
         except git.exc.GitError as err:
             print(err)
+
+
+###############################################################################
+# Main script                                                                 #
+###############################################################################
+
+def main():
+
+    install_python_pkgs()
+    install_pkgs()
+    install_repos()
 
 
 if __name__ == '__main__':
